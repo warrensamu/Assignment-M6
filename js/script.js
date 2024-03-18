25b2533005aa737efdc9d01cd0445f10729c5a75
warrensamu <116026091+warrensamu@users.noreply.github.com>
2024-03-18 12:50:04 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const $ = (id) => document.getElementById(id)
+let id          = $('id')
+let name        = $('name')
+let ext         = $('ext')
+let email       = $('email')
+let department  = $('department')
+
+document.getElementById('empForm').addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(`ID: ${id.value}`)
+    console.log(`Name: ${name.value}`)
+    console.log(`Extension: ${ext.value}`)
+    console.log(`Email: ${email.value}`)
+    console.log(`Department: ${department.value}`)
+})
